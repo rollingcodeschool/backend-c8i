27642062fbabfa180db09d3fd466123323c323e3
Emilse Arias <emilsearias@gmail.com>
2022-10-19 18:58:32 -0300

    agregamos los middlewares y comando de desarrollo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan'
 
 //crear una instancia de express
 const app = express();
@@ -9,4 +11,11 @@ app.listen( app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port'))
 })
 
-console.log('hola mundo')
+//midlewares: funciones que se ejecutan antes de las rutas
+app.use(cors()); //permite conexiones remotas
+
+//permiten interpretar el formato json
+app.use(express.json());
+app.use(express.urlencoded({extends:true}))
+
+//rutas
