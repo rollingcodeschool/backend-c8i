b6d53a432891c796a8cee4ab3319d98469fcfd36
Emilse Arias <emilsearias@gmail.com>
2022-10-26 17:21:28 -0300

    agregamos la logica para obtener un producto mediante el id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -13,6 +13,21 @@ export const listarProductos = async(req, res)=>{
     })
    }
 }
+export const obtenerProducto = async(req, res)=>{
+   try{
+       //extraer el id de la ruta
+        console.log(req.params.id)
+       //buscar en la BD el producto que coincida con el id
+        const productoBuscado = await Producto.findById(req.params.id);
+       //respondes con el producto encontrado
+        res.status(200).json(productoBuscado);
+   }catch(error){
+    console.log(error);
+    res.status(404).json({
+        mensaje: 'Error no se encontro el producto buscado'
+    })
+   }
+}
 
 export const crearProducto = async(req, res)=>{
     try{
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { crearProducto, listarProductos } from '../controllers/productos.controllers';
+import { crearProducto, listarProductos, obtenerProducto } from '../controllers/productos.controllers';
 
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProducto);
+router.route('/productos/:id').get(obtenerProducto)
 
 // app.get('/prueba',(req, res)=>{
 //     res.send('esto es una prueba de una peticion get')
